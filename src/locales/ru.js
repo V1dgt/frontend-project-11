19e4af0cfe2247155755fbe3f40f764ee0c95ac2
v1dgt <v1dgt@mail.ru>
2025-11-26 13:46:08 +0300

    add locales

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  ru: {
+    translation: {
+      success: 'RSS успешно загружен',
+      errors: {
+        url_invalid: 'Ссылка должна быть валидным URL',
+        url_exists: 'RSS уже существует',
+      },
+    },
+  },
+}
