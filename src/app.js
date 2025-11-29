b24803da8f555769221f8926926ea4aec4f5d143
v1dgt <v1dgt@mail.ru>
2025-11-29 19:19:32 +0300

    upd app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+/* eslint-disable @stylistic/quotes */
 /* eslint-disable no-undef */
-import createWatcher from './watcher'
+import createWatcher from './watcher.js'
 import * as yup from 'yup'
 import i18n from 'i18next'
-import resources from './locales/ru.js'
+import resources from './locales/resources.js'
+import parser from './parser.js'
+import axios from 'axios'
+import { uniqueId } from 'lodash'
 
-const shemaUrl = yup.string().url('Ссылка должна быть валидным URL')
+const shemaUrl = yup.string().url('url_invalid')
 
 const validateUniqUrl = (url, oldUrl) => {
   if (url === oldUrl) {
@@ -18,6 +22,27 @@ yup.setLocale({
     url: 'url_invalid',
   },
 })
+
+const processingFeed = (data) => {
+  const { feed } = data
+  const idFeed = uniqueId()
+  feed.id = idFeed
+  return feed
+}
+
+const processingPosts = (data, feed) => {
+  const { posts } = data
+  const editPosts = [...posts].map((post) => {
+    const title = post.querySelector('title').textContent
+    const description = post.querySelector('description').textContent
+    const link = post.querySelector('link').textContent
+    const idPost = uniqueId()
+    const idFeed = feed.id
+    const newPost = { idPost, title, description, link, idFeed }
+    return newPost
+  })
+  return editPosts
+}
 
 const initApp = () => {
   const defaultLanguage = 'ru'
@@ -35,6 +60,7 @@ const initApp = () => {
           errors: [],
         },
         feeds: [],
+        posts: [],
       }
 
       const watchedState = createWatcher(state, i18Instance)
@@ -59,15 +85,30 @@ const initApp = () => {
             }
           })
           .then(() => {
-            watchedState.url = inputUrl
+            const url = encodeURIComponent(`${inputUrl}`)
+            const responce = axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
+            return responce
+          })
+          .then((responce) => {
+            const parseFeedAndPosts = parser(responce.data.contents)
+            const feed = processingFeed(parseFeedAndPosts)
+            const posts = processingPosts(parseFeedAndPosts, feed)
+            watchedState.feeds.push(feed)
+            watchedState.posts.push(...posts)
+          })
+          .then(() => {
             watchedState.form.status = 'success'
+          })
+          .then(() => {
+            watchedState.url = inputUrl
           })
           .then(() => {
             watchedState.form.status = 'filling'
           })
           .catch((error) => {
+            const messageError = error.message.toLowerCase().replaceAll(" ", "")
             watchedState.form.status = 'error'
-            watchedState.form.errors.push(i18Instance.t(error.message))
+            watchedState.form.errors.push(i18Instance.t(messageError))
           })
       })
     })
