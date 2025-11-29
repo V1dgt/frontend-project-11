d6793b8b4d016f7a0fe2b69e5debfaf7a42ff1f3
v1dgt <v1dgt@mail.ru>
2025-11-29 19:21:37 +0300

    upd parser.js

---
+++ src/parser.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+const parser = (rssString) => {
+  const domParser = new DOMParser()
+  const doc = domParser.parseFromString(rssString, 'application/xml')
+  const errorNode = doc.querySelector('parsererror')
+  if (errorNode) {
+    throw new Error('parse_error')
+  }
+  else {
+    const title = doc.querySelector('channel > title').textContent
+    const description = doc.querySelector('channel > description').textContent
+    const feed = { title, description }
+    const posts = doc.querySelectorAll('item')
+    const feedAndPosts = { feed, posts }
+    return feedAndPosts
+  }
+}
+
+export default parser
