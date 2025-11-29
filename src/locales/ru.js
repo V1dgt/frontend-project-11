704673c95de236b6870f315d02bc964d83512481
v1dgt <v1dgt@mail.ru>
2025-11-29 19:20:11 +0300

    upd ru.js

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,11 +1,16 @@
 export default {
-  ru: {
-    translation: {
+  translation: {
+    errors: {
+      url_invalid: 'Ссылка должна быть валидным URL',
+      url_exists: 'RSS уже существует',
+      networkerror: 'Ошибка сети',
+      parse_error: 'Ошибка парсинга',
+    },
+    other_words: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      viewing: 'Просмотр',
       success: 'RSS успешно загружен',
-      errors: {
-        url_invalid: 'Ссылка должна быть валидным URL',
-        url_exists: 'RSS уже существует',
-      },
     },
   },
 }
